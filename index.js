941db1572cde0c682991853b352eca2f1501b6ea
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-09-23 11:20:40 +0200

    move cards content to js

--- index.js
+++ index.js
@@ -16,11 +16,46 @@ const product3 = {
   description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
 
+const product1HTML = `
+  <img class="card-img-top" src="${product1.image}" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product1.name}</h2>
+    <p class="card-text">${product1.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+
+const product2HTML = `
+  <img class="card-img-top" src="${product2.image}" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product2.name}</h2>
+    <p class="card-text">${product2.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+
+const product3HTML = `
+  <img class="card-img-top" src="${product3.image}" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product3.name}</h2>
+    <p class="card-text">${product3.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+
 const card1 = document.querySelector('#product1');
+card1.innerHTML = product1HTML;
+
+const card2 = document.querySelector('#product2');
+card2.innerHTML = product2HTML;
+
+const card3 = document.querySelector('#product3');
+card3.innerHTML = product3HTML;
+
 card1.classList.add('border-primary');
 
-const card2Btn = document.querySelector('#product2 button');
+const card2Btn = card2.querySelector('button');
 card2Btn.classList.remove('btn-primary');
 
-const card3Title = document.querySelector('#product3 .card-title');
+const card3Title = card3.querySelector('.card-title');
 card3Title.classList.toggle('text-center');
